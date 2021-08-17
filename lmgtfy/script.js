b37bb6b6b83e7e43fcf35b38ff15b19f9db45a22
Rian <49768896+DatRatVS@users.noreply.github.com>
2021-08-16 22:22:09 -0300

    Listening to the Enter Key Since 08/16 22:22

--- lmgtfy/script.js
+++ lmgtfy/script.js
@@ -4,3 +4,13 @@ function onClick() {
     console.log("Redirecting to: https://www.google.com/search?q=" + replacedText);
     window.location.href = "https://www.google.com/search?q=" + replacedText;
 }
+
+const node = document.getElementsByClassName("textbox")[0];
+node.addEventListener("keyup", function(event) {
+    if (event.key === "Enter") {
+        let textInput = document.getElementById("textbox").value;
+        let replacedText = textInput.replace(/ /gi, "+");
+        console.log("Redirecting to: https://www.google.com/search?q=" + replacedText);
+        window.location.href = "https://www.google.com/search?q=" + replacedText;
+    }
+});
