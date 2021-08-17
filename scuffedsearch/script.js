130f150eeb1330a3171498b9fbeb9e5f800b6c8d
Rian <49768896+DatRatVS@users.noreply.github.com>
2021-08-16 23:53:16 -0300

    if null, dont do anything

--- scuffedsearch/script.js
+++ scuffedsearch/script.js
@@ -1,8 +1,13 @@
 function onClick() {
     let textInput = document.getElementById("textbox").value;
     let replacedText = textInput.replace(/ /gi, "+");
-    console.log("Redirecting to: https://www.google.com/search?q=" + replacedText);
-    window.location.href = "https://www.google.com/search?q=" + replacedText;
+
+    if (textInput == "") {
+        console.log("Nothing on textbox, not doing anything.");
+    } else {
+        console.log("Redirecting to: https://www.google.com/search?q=" + replacedText);
+        window.location.href = "https://www.google.com/search?q=" + replacedText;
+    }
 }
 
 function onClickWaiting() {
@@ -18,7 +23,12 @@ node.addEventListener("keyup", function(event) {
     if (event.key === "Enter") {
         let textInput = document.getElementById("textbox").value;
         let replacedText = textInput.replace(/ /gi, "+");
-        console.log("Redirecting to: https://www.google.com/search?q=" + replacedText);
-        window.location.href = "https://www.google.com/search?q=" + replacedText;
+
+        if (textInput == "") {
+            console.log("Nothing on textbox, not doing anything.");
+        } else {
+            console.log("Redirecting to: https://www.google.com/search?q=" + replacedText);
+            window.location.href = "https://www.google.com/search?q=" + replacedText;
+        }
     }
 });
