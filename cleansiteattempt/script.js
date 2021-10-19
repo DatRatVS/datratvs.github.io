a6b0d06cd9d73ca29e48c909f7e63320342f0d87
Rian <49768896+DatRatVS@users.noreply.github.com>
2021-10-19 20:53:53 -0300

    Clean Site Attempt

---
+++ cleansiteattempt/script.js
@@ -0,0 +1,8 @@
+function sideBar() {
+  document.getElementById("containerID").classList.toggle("change")
+  document.getElementById("sideBarID").classList.toggle("change1")
+}
+
+function hrefTransition() {
+  document.getElementById("transitionBlackScreenID").classList.toggle("transitionedBlackScreen")
+}
