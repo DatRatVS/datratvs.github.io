01de1e1ba9ed4c3248e7f825f57ff7bffe7a6144
Rian <49768896+DatRatVS@users.noreply.github.com>
2021-12-11 20:57:43 -0300

    Even More Morals

--- bemvindoaosite/noredir/script.js
+++ bemvindoaosite/noredir/script.js
@@ -1,6 +1,7 @@
 function onClick() {
   let video = document.getElementById('video');
   video.style.display = 'block';
+  video.volume = 0.05;
   video.play();
   setTimeout(() => {  window.location.href = "https://datratvs.github.io/bemvindoaosite/noredir/redir.md"; }, 15500);
 }
